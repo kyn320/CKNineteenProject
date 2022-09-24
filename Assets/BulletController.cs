b54e96db88c77dda6bf5169cccbcf910520ab1a8
dayoff53 <42767781+dayoff53@users.noreply.github.com>
2022-09-24 22:07:43 +0900

    줌 오류 수정 및 총알 발사 시 보는 방향으로 날아감

--- Assets/BulletController.cs
+++ Assets/BulletController.cs
@@ -8,6 +8,7 @@ public class BulletController : MonoBehaviour
     public float bulletPower;
     public Vector3 bulletStartVecotr;
     public GameObject cameraActhor;
+    public GameObject mainCamera;
 
 
 
@@ -18,12 +19,21 @@ public class BulletController : MonoBehaviour
         {
             Debug.LogError($"{this.gameObject.name} has not bullet");
         }
+
         if (cameraActhor == null)
         {
             cameraActhor = transform.Find("Camera Acthor").gameObject;
 
             if (cameraActhor == null)
                 Debug.LogError($"{this.gameObject.name} has not cameraActhor");
+        }
+
+        if (mainCamera == null)
+        {
+            mainCamera = GameObject.Find("Main Camera");
+
+            if (mainCamera == null)
+            Debug.LogError($"{this.gameObject.name} has not mainCamera");
         }
     }
 
@@ -32,12 +42,21 @@ public class BulletController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            Vector3 bulletNewVecotr = bulletStartVecotr + transform.position;
+            Vector3 cameraFrontVector = cameraActhor.GetComponent<CameraMoveController>().cameraFrontVector;
+
+
             //ÃÑ¾Ë Ãâ·Â À§Ä¡
-            Vector3 bulletNewVecotr = new Vector3(bulletStartVecotr.x + transform.position.x, bulletStartVecotr.y + transform.position.y, bulletStartVecotr.z + transform.position.z);
             GameObject goBullet = Instantiate(bullet, bulletNewVecotr, new Quaternion(0, 0, 0, 0));
 
+
+
+            /*
+            float x = Mathf.Abs(cameraActhor.transform.localPosition.x + cameraActhor.transform.localPosition.x);
+            float y = Mathf.Abs(cameraActhor.transform.localPosition.y + cameraActhor.transform.localPosition.y);
+            */
+
             //ÃÑ¾Ë ¹°¸® º¤ÅÍ
-            Vector3 cameraFrontVector = cameraActhor.GetComponent<CameraMoveController>().cameraFrontVector;
             goBullet.GetComponent<Rigidbody>().velocity = cameraFrontVector.normalized * bulletPower;
         }
     }
