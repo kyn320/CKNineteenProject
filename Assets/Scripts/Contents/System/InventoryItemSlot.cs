b415348c5b3e74e47978ecf70d6364080b781cd4
zerosouth <kyns320@gmail.com>
2022-09-27 12:12:56 +0900

    [feat] 아이템 데이터 테이블 구축

---
+++ Assets/Scripts/Contents/System/InventoryItemSlot.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItemSlot : MonoBehaviour
+{
+
+    public ItemData itemData;
+    public int amount;
+
+}
