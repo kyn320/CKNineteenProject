9f47a0e6ca8180c1e0b08fd846d843be42e177b5
Knayul <ssoupni15963@gmail.com>
2022-11-04 11:25:19 +0900

    [feat] Bezier Curve Undo 기능 추가

--- Assets/Editor/Curve/BezierCurveEditor.cs
+++ Assets/Editor/Curve/BezierCurveEditor.cs
@@ -7,15 +7,25 @@ using UnityEditor;
 [CustomEditor(typeof(BezierCurve))]
 public class BezierCurveEditor : OdinEditor
 {
+    class UndoPointData
+    {
+        public Vector3 anchor;
+        public Vector3 leftHandle;
+        public Vector3 rightHandle;
+    }
+
+    private List<UndoPointData> changePointList = new List<UndoPointData>();
 
     private void OnSceneGUI()
     {
         Tools.current = Tool.None;
 
         var bezierCurve = target as BezierCurve;
+        changePointList.Clear();
 
-        bezierCurve.transform.position = Handles.DoPositionHandle(bezierCurve.transform.position, Quaternion.identity);
+        EditorGUI.BeginChangeCheck();
 
+        var rootPivot = Handles.PositionHandle(bezierCurve.transform.position, Quaternion.identity);
         var pointList = bezierCurve.GetPointList();
 
         for (var i = 0; i < pointList.Count; ++i)
@@ -24,9 +34,39 @@ public class BezierCurveEditor : OdinEditor
             var leftHandle = pointList[i].GetLeftHandle();
             var rightHandle = pointList[i].GetRightHandle();
 
-            anchor.position = Handles.DoPositionHandle(anchor.position, Quaternion.identity);
-            leftHandle.position = Handles.DoPositionHandle(leftHandle.position, Quaternion.identity);
-            rightHandle.position = Handles.DoPositionHandle(rightHandle.position, Quaternion.identity);
+            changePointList.Add(new UndoPointData()
+            {
+                anchor = Handles.PositionHandle(anchor.position, Quaternion.identity)
+            ,
+                leftHandle = Handles.PositionHandle(leftHandle.position, Quaternion.identity)
+            ,
+                rightHandle = Handles.PositionHandle(rightHandle.position, Quaternion.identity)
+            });
+        }
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.SetCurrentGroupName("Modify BezierCurve");
+            int group = Undo.GetCurrentGroup();
+
+            Undo.RegisterCompleteObjectUndo(bezierCurve.transform, "Modify BezierCurve : rootPivot");
+            for (var i = 0; i < pointList.Count; ++i)
+            {
+                var anchor = pointList[i].GetAnchor();
+                var leftHandle = pointList[i].GetLeftHandle();
+                var rightHandle = pointList[i].GetRightHandle();
+
+
+                Undo.RegisterCompleteObjectUndo(anchor.transform, "Modify BezierCurve : anchor");
+                Undo.RegisterCompleteObjectUndo(leftHandle.transform, "Modify BezierCurve : leftHandle");
+                Undo.RegisterCompleteObjectUndo(rightHandle.transform, "Modify BezierCurve : rightHandle");
+
+                anchor.position = changePointList[i].anchor;
+                leftHandle.position = changePointList[i].leftHandle;
+                rightHandle.position = changePointList[i].rightHandle;
+            }
+            bezierCurve.transform.position = rootPivot;
+            Undo.CollapseUndoOperations(group);
         }
 
     }
